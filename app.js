d23defa07d90603c258f845363f03735f0c90b0f
rudyravelindev <rudyravelindev@gmail.com>
2025-06-11 19:46:57 -0700

    Part 4: Mongoose Connection and tested done

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const { PORT = 3001 } = process.env;
 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(console.error);
+
 app.listen(PORT, () => {
-  console.log(`ÍListening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
