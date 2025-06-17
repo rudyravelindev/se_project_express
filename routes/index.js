4e1fa18c00da3aed899662941bf24d4613bad973
rudyravelindev <rudyravelindev@gmail.com>
2025-06-16 18:28:02 -0700

    Importing the Itmes routes and connetction and i=use of CONSTNAT

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
 
 const userRouter = require("./users");
+const clothingItemsRouter = require("./clothingItems");
+const { NOT_FOUND } = require("../utils/errors");
 
+// Mount route handlers
 router.use("/users", userRouter);
+router.use("/items", clothingItemsRouter);
+
+// Handle unknown routes (404)
+router.use((req, res) => {
+  res.status(NOT_FOUND).send({ message: "Requested resource not found" });
+});
 
 module.exports = router;
