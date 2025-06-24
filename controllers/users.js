95bc43f98d455c760eb3e34fbc3a32c2ee3b5381
rudyravelindev <rudyravelindev@gmail.com>
2025-06-23 22:40:27 -0700

    expand the user schema and updating the createUser controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,15 @@
 const User = require("../models/user");
-const { BAD_REQUEST, NOT_FOUND, SERVER_ERROR } = require("../utils/errors");
+const {
+  BAD_REQUEST,
+  NOT_FOUND,
+  SERVER_ERROR,
+  CONFLICT,
+} = require("../utils/errors");
+const bcrypt = require("bcryptjs");
 
 const getUsers = (req, res) => {
   User.find({})
+    .select("-password")
     .then((users) => {
       res.status(200).send(users);
     })
@@ -14,24 +21,46 @@ const getUsers = (req, res) => {
     });
 };
 
-const createUser = (req, res) => {
-  const { name, avatar } = req.body;
-  User.create({ name, avatar })
-    .then((user) => res.status(201).send(user))
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "ValidationError") {
-        return res.status(BAD_REQUEST).send({ message: "Invalid data" });
-      }
-      return res
-        .status(SERVER_ERROR)
-        .send({ message: "An error has occurred on the server." });
+const createUser = async (req, res) => {
+  const { name, avatar, email, password } = req.body;
+
+  try {
+    // Create user - password hashing happens automatically via the pre('save') hook
+    const user = await User.create({ name, avatar, email, password });
+
+    // Remove password from response
+    const userResponse = user.toObject();
+    delete userResponse.password;
+
+    res.status(201).send(userResponse);
+  } catch (err) {
+    console.error("Error creating user:", err);
+
+    if (err.code === 11000) {
+      return res.status(409).send({
+        message: "Email already exists",
+        error: err.message,
+      });
+    }
+
+    if (err.name === "ValidationError") {
+      return res.status(400).send({
+        message: "Validation failed",
+        errors: Object.values(err.errors).map((e) => e.message),
+      });
+    }
+
+    res.status(500).send({
+      message: "Internal server error",
+      error: err.message,
     });
+  }
 };
 
 const getUser = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
+    .select("-password")
     .orFail()
     .then((user) => res.status(200).send(user))
     .catch((err) => {
@@ -40,7 +69,9 @@ const getUser = (req, res) => {
         return res.status(NOT_FOUND).send({ message: "User not found" });
       }
       if (err.name === "CastError") {
-        return res.status(BAD_REQUEST).send({ message: "Invalid data" });
+        return res
+          .status(BAD_REQUEST)
+          .send({ message: "Invalid user ID format" });
       }
       return res
         .status(SERVER_ERROR)
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -18,6 +19,36 @@ const userSchema = new mongoose.Schema({
       message: "You must enter a valid URL",
     },
   },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+      message: "You must enter a valid email address",
+    },
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [8, "Password must be at least 8 characters long"],
+    select: false,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = mongoose.model("user", userSchema);
