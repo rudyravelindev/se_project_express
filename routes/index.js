6909645f80695a32949d1bdfdc169120ec9da484
rudyravelindev <rudyravelindev@gmail.com>
2025-08-06 22:02:57 -0700

    middleware for handling an unknown

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,5 @@
 const router = require("express").Router();
 const { login, createUser } = require("../controllers/users");
-const {
-  getItems,
-  createItem,
-  getItemById,
-  deleteItem,
-  likeItem,
-  dislikeItem,
-} = require("../controllers/clothingItems"); // Added missing exports
 const auth = require("../middlewares/auth");
 const {
   validateCardBody,
@@ -15,28 +7,19 @@ const {
   validateLogin,
   validateId,
 } = require("../middlewares/validation");
-const { NOT_FOUND } = require("../utils/errors");
+const { NotFoundError } = require("../utils/errors"); // Import the error class
 
 // Public routes
 router.post("/signin", validateLogin, login);
 router.post("/signup", validateUserBody, createUser);
-router.get("/items", getItems);
-
-// Protected routes (require authentication)
-router.use(auth);
-router.post("/items", validateCardBody, createItem);
-router.get("/items/:id", validateId, getItemById);
-router.delete("/items/:id", validateId, deleteItem); // Added missing route
-router.put("/items/:id/likes", validateId, likeItem); // Added missing route
-router.delete("/items/:id/likes", validateId, dislikeItem); // Added missing route
 
 // Mount sub-routers
-router.use("/users", require("./users"));
 router.use("/items", require("./clothingItems"));
+router.use("/users", auth, require("./users"));
 
-// 404 handler
-router.use((req, res) => {
-  res.status(NOT_FOUND).send({ message: "Requested resource not found" });
+// 404 handler - now throws error instead of sending response directly
+router.use((req, res, next) => {
+  next(new NotFoundError("Requested resource not found"));
 });
 
 module.exports = router;
